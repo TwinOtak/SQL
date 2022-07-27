a17cc29938f353d400481706bf4fde97e8cf88a9
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-07-27 15:18:13 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,2 +1,7 @@
 select *
 from actor
+
+select	'abc' like 'abc' as "true or false",
+		'abc' like 'a%' as "true or false",
+		'abc' like '_b_' as "true or false",
+		'abc' like 'c' as "true or false";
