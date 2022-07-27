c9560ea61977da218afe9d2f5c87544f8796f4f5
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-07-27 15:18:08 +0300

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT 
+INTO	Authors (au_id, au_fname, au_lname, city, contract)
+VALUES	('111-22-3333', 'Евгений', 'Онегин', 'Ленинград', 1),
+		('111-22-3334', 'Владимир', 'Ленский', 'Москва', 0)
