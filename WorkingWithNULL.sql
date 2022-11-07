3f7a03225155451d70be6e7a8f64bfd25c587fab
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-07 15:44:54 +0300

    Update WorkingWithNULL.sql

--- WorkingWithNULL.sql
+++ WorkingWithNULL.sql
@@ -2,3 +2,7 @@ SELECT	ContactName
 FROM	customers
 WHERE	city = 'London'
 	AND	fax IS NOT NULL
+
+SELECT	Title + '(' + Notes + ')', 
+		Title + '(' + IsNull(Notes, 'Нет описания') + ')'
+FROM	titles
