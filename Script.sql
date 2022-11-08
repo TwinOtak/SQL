5d189ea6b85fb847483e84103662b4d007b9b834
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-08 04:34:55 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -5,8 +5,3 @@ select	'abc' like 'abc' as "true or false",
 		'abc' like 'a%' as "true or false",
 		'abc' like '_b_' as "true or false",
 		'abc' like 'c' as "true or false";
-
-select	table_name, constraint_name
-from	information_schema.table_constraints
-where 	constraint_schema = 'public'
-	and	constraint_type = 'PRIMARY KEY'
