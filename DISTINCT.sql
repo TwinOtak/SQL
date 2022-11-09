fca2fb8cd50314814ee181bd2a9d23f7f73aa150
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-09 04:16:25 +0300

    Create DISTINCT.sql

---
+++ DISTINCT.sql
@@ -0,0 +1,9 @@
+﻿SELECT	DISTINCT Country, City
+FROM	Customers
+
+
+--В какие города оформлял заказы продавец №1 в 1996 году?
+SELECT TOP(100) ShipCity
+FROM	Orders
+WHERE	EmployeeID = 1
+	AND	YEAR(OrderDate) = 1996
