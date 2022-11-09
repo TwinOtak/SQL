51eacfdd48f3a6d8ae72ca67a75695d1760d8d97
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-09 04:05:18 +0300

    Update TOP.sql

--- TOP.sql
+++ TOP.sql
@@ -1,3 +1,13 @@
 ﻿SELECT	TOP(1) ProductName, UnitPrice
 FROM	Products
 ORDER BY	UnitPrice DESC	--по убыванию
+
+SELECT	TOP(11) WITH TIES ProductName, UnitPrice --Добирает дубликаты значений, чтобы их учитывать
+FROM	Products
+ORDER BY	UnitPrice DESC	--по убыванию
+
+--Выводит самого молодого сотрудника из лондона
+SELECT	TOP(1) WITH TIES *
+FROM	Employees
+WHERE	City = 'London'
+ORDER BY	BirthDate DESC --по возрастанию
