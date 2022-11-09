d44020976916be94b40ec77a831d6165128329bb
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-09 04:05:16 +0300

    Create EXERCISE.sql

---
+++ EXERCISE.sql
@@ -0,0 +1,6 @@
+﻿-- В какую страну был оформлен последний заказ осенью 1997 года?
+SELECT	TOP(1) ShipCountry
+FROM	Orders
+WHERE	YEAR(OrderDate) = 1997
+	AND	MONTH(OrderDate) BETWEEN 9 AND 11
+ORDER BY OrderDate DESC
