c78c37fa2b7d14706224f0a9cc74cadcac9fde54
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-09 04:16:23 +0300

    Update EXERCISE.sql

--- EXERCISE.sql
+++ EXERCISE.sql
@@ -4,3 +4,9 @@ FROM	Orders
 WHERE	YEAR(OrderDate) = 1997
 	AND	MONTH(OrderDate) BETWEEN 9 AND 11
 ORDER BY OrderDate DESC
+
+--В какие города оформлял заказы продавец №1 в 1996 году?
+SELECT TOP(100) ShipCity
+FROM	Orders
+WHERE	EmployeeID = 1
+	AND	YEAR(OrderDate) = 1996
