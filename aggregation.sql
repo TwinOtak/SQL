2bd76bf1a5b4a248ba7b7ef7731924913d8e749e
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-10 01:55:36 +0300

    Update aggregation.sql

--- aggregation.sql
+++ aggregation.sql
@@ -1,2 +1,16 @@
 ﻿SELECT	Avg(Price), Min(Price), Max(Price), Sum(Price)
 FROM	titles
+
+SELECT	Avg(Price), Avg(Price * (1 + 0.18)) --Можно передавать производные значения
+FROM	titles
+
+SELECT	Count(*), Count(Price) --1. Считает строки с Null; 2. Считает строки со значениями в выбранном столбце
+FROM	titles
+
+
+SELECT	Avg(Price)	--Сначала срабатывает WHERE
+FROM	titles
+WHERE	type = 'business'
+
+----------------------------------------------------------------------
+
