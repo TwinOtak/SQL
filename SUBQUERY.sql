18550e826821cdc1cc86da05c53ff69b648d3220
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-10 19:04:06 +0300

    Update SUBQUERY.sql

--- SUBQUERY.sql
+++ SUBQUERY.sql
@@ -3,10 +3,10 @@
 --Каждая строка в ответе, это продавец
 --Если я хочу получить список продавцов, то к нему и пишу select
 --Как только сталкиваюсь с нехваткой данных, ставлю () и внутри пишу подзапрос
-SELECT	EmployeeID, FirstName, LastName,
-		(
-		SELECT		COUNT(*)
-		FROM		Orders
-		WHERE		EmployeeID = Employees.EmployeeID
-		)
-FROM	Employees
+SELECT		EmployeeID, FirstName, LastName,
+			(
+			SELECT		COUNT(*)
+			FROM		Orders
+			WHERE		EmployeeID = Employees.EmployeeID
+			)
+FROM		Employees
