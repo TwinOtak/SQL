c9564f971c344eb03545f0f363568f50a716c690
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-10 15:11:54 +0300

    Update EXERCISE.sql

--- EXERCISE.sql
+++ EXERCISE.sql
@@ -80,3 +80,18 @@ SELECT		TOP(1) WITH TIES EmployeeID--, COUNT(DISTINCT CustomerID)
 FROM		Orders
 GROUP BY	YEAR(OrderDate), MONTH(OrderDate), EmployeeID, ShipCountry
 ORDER BY	COUNT(DISTINCT CustomerID) DESC
+
+----------------------------------------------------------------------
+
+--Какие товары принесли выручку больше 10.000?
+SELECT		DISTINCT ProductID--, SUM(UnitPrice*Quantity*(1-Discount)) AS Total
+FROM		[Order Details]
+GROUP BY	ProductID
+HAVING		SUM(UnitPrice*Quantity*(1-Discount)) > 10000
+
+--Какаие продавцы в 1997 году сумели обслужить больше пяти городов в одной стране?
+SELECT		DISTINCT EmployeeID--, ShipCountry, COUNT(DISTINCT ShipCity)
+FROM		Orders
+WHERE		YEAR(OrderDate) = 1997
+GROUP BY	EmployeeID, ShipCountry
+HAVING		COUNT(DISTINCT ShipCity) > 5
