629a6ff648dec97d7759bd3d75f09d67160fd68f
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-10 19:02:29 +0300

    Create SUBQUERY.sql

---
+++ SUBQUERY.sql
@@ -0,0 +1,12 @@
+﻿--SUBQUERY
+--Сколько заказов оформил каждый продавец (ФИО)?
+--Каждая строка в ответе, это продавец
+--Если я хочу получить список продавцов, то к нему и пишу select
+--Как только сталкиваюсь с нехваткой данных, ставлю () и внутри пишу подзапрос
+SELECT		EmployeeID, FirstName, LastName,
+			(
+			SELECT		COUNT(*)
+			FROM		Orders
+			WHERE		 EmployeeID = Employees.EmployeeID
+			)
+FROM		Employees
