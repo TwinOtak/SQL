313cd5a7ec3b83b6a032966b9049573b7347d556
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-11 02:43:02 +0300

    Update EXERCISE.sql

--- EXERCISE.sql
+++ EXERCISE.sql
@@ -158,3 +158,33 @@ WHERE		YEAR(OrderDate) = 1996
 
 ------------------------------------------------------------------------------------
 
+--SUBQUERY
+--Сколько заказов оформил каждый продавец (ФИО)?
+--Каждая строка в ответе, это продавец
+--Если я хочу получить список продавцов, то к нему и пишу select
+--Как только сталкиваюсь с нехваткой данных, ставлю () и внутри пишу подзапрос
+SELECT		EmployeeID, FirstName, LastName,
+			(
+			SELECT		COUNT(*)
+			FROM		Orders
+			WHERE		EmployeeID = Employees.EmployeeID
+			)
+FROM		Employees
+
+--Посчитать среднюю цену товара для каждой категории
+SELECT		CategoryName,
+			(
+			SELECT		AVG(UnitPrice)	--Нужно чтобы возвращал только 1 значение
+			FROM		Products
+			WHERE		CategoryID = Categories.CategoryID
+			) AS AveragePrice
+FROM		Categories
+
+--Посчитать выручку с каждого товара (Название)
+SELECT		ProductName,
+			(
+			SELECT		SUM(UnitPrice*Quantity*(1-Discount))
+			FROM		[Order Details]
+			WHERE		ProductID = Products.ProductID
+			)  AS INCOME
+FROM		Products
