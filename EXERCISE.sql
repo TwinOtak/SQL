c8582efcc02d9f71c322a1585bb5baba78b278d3
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-12 21:31:32 +0300

    Update EXERCISE.sql

--- EXERCISE.sql
+++ EXERCISE.sql
@@ -395,3 +395,16 @@ FROM		Employees AS E LEFT JOIN Orders AS O	ON E.EmployeeID = O.EmployeeID
 		AND	ShipCity = 'London'
 		AND ProductID = 1	--Дополнительно
 GROUP BY	FirstName + ' ' + LastName
+
+
+--Сколько штук каждого товара (Название) мы продали в августе 1996 года?
+SELECT	ProductName, IsNull(SUM(Quantity),0)
+FROM	Products AS P LEFT JOIN 
+		(
+		[Order Details] AS OD INNER JOIN Orders AS O
+		ON OD.OrderID = O.OrderID
+		AND YEAR(OrderDate) = 1996
+		AND MONTH(OrderDate) = 8
+		)
+		ON P.ProductID = OD.ProductID
+GROUP BY	ProductName
