b3d32866e4a94d707f0e2ced2cd5031305d8bb04
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-12 22:30:34 +0300

    Update APPLY.sql

--- APPLY.sql
+++ APPLY.sql
@@ -33,3 +33,41 @@ ORDER BY CategoryName
 --Решение абсолютно не гибкое, тк если нас попросят вытащить 50 товаров, то придется 50 раз копировать вторую часть и менять там ТОП
 --Нужно чтобы можно было задавать переменной
 --Для таких ситуаций и придуман APPLY
+--Пример
+SELECT	C.CategoryName, P.ProductName
+FROM	Categories AS C CROSS APPLY	(
+									SELECT	TOP(2) *
+									FROM	Products
+									WHERE	CategoryID = C.CategoryID
+									ORDER BY UnitPrice DESC
+									) P
+--Добавляем with ties
+SELECT	C.CategoryName, P.ProductName, UnitPrice
+FROM	Categories AS C CROSS APPLY	(
+									SELECT	TOP(4) WITH TIES *
+									FROM	Products
+									WHERE	CategoryID = C.CategoryID
+									ORDER BY UnitPrice DESC
+									) P
+
+--Для каждого продавца показать три его любимых города? (Город в который он чаще всего продает заказы)
+--Продавцы - Список №1 (Его перебираем), Города - Список №2 (Его обновляем). Начинаем решать со второго списка.
+--Строим второй список
+SELECT	TOP(3) WITH TIES ShipCity, COUNT(*)
+FROM	Orders
+WHERE	EmployeeID = 2
+GROUP BY ShipCity
+ORDER BY COUNT(*) DESC
+--Вот три любимых города для продавца №2. (Для примера: У него есть одинаковые значения)
+--Теперь формируем первый список
+SELECT	FirstName + ' ' + LastName
+FROM	Employees
+--Теперь соединяем списки через CROSS APPLY
+SELECT	FirstName + ' ' + LastName, ShipCity
+FROM	Employees AS E	CROSS APPLY (
+									SELECT	TOP(3) WITH TIES ShipCity
+									FROM	Orders
+									WHERE	EmployeeID = E.EmployeeID
+									GROUP BY ShipCity
+									ORDER BY COUNT(*) DESC
+									) AS C
