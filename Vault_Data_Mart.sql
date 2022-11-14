d103c4f1e4c6ca239b6e586ec1d70f5423419ce0
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-14 21:50:12 +0300

    Create Vault_Data_Mart.sql

---
+++ Vault_Data_Mart.sql
@@ -0,0 +1,13 @@
+﻿SELECT		EnglishMonthName,
+			EnglishOccupation,
+			SUM(SalesAmount)
+FROM		FactInternetSales AS I	--Берем таблицу фактов
+			INNER JOIN DimCustomer AS C	--Джоиним к ней таблицу размерностей
+			ON	I.CustomerKey = C.CustomerKey
+			INNER JOIN DimDate as D -- и еще джоиним к ней таблицу размерностей
+			ON I.OrderDateKey = D.DateKey
+WHERE		D.CalendarYear = 2011
+		AND	C.Gender = 'F'
+GROUP BY	EnglishMonthName,
+			EnglishOccupation
+ORDER BY	SUM(SalesAmount) DESC
