6567f5ad13f2a1294043d56fef11fe4e31cf5207
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-14 18:39:16 +0300

    Update Window_Aggregation.sql

--- Window_Aggregation.sql
+++ Window_Aggregation.sql
@@ -1,4 +1,5 @@
-﻿SELECT		OrderID, Freight, 
+﻿--Window Aggregarion
+SELECT		OrderID, Freight, 
 			SUM(Freight) OVER	(
 								ORDER BY OrderID ASC
 								ROWS BETWEEN	--Задаем границы окна
@@ -6,5 +7,17 @@
 									AND
 									CURRENT ROW			--Нижняя граница (Скользит с текущей строкой)
 								)
+FROM		Orders
+ORDER BY	OrderID
+
+--Moving average
+SELECT		OrderID, Freight,
+			AVG(Freight)	OVER	(
+									ORDER BY	OrderID ASC
+									ROWS BETWEEN
+										10 PRECEDING
+										AND
+										10 FOLLOWING
+									)
 FROM		Orders
 ORDER BY	OrderID
