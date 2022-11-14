cb813eaee5a66a1aad8b1fa5e9c7bc4eccebdba1
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-15 00:02:23 +0300

    Create check.sql

---
+++ check.sql
@@ -0,0 +1,44 @@
+﻿--Как зовут продавцов, которые оформили заказов в Берлин больше, чем в Париж?
+--1 --Подзапрос
+SELECT		FirstName + ' ' + LastName as Name
+FROM		Employees
+WHERE		(
+			SELECT		COUNT(*)
+			FROM		Orders
+			WHERE		EmployeeID = Employees.EmployeeID
+					AND	ShipCity = 'Berlin'
+			)
+			>
+			(
+			SELECT		COUNT(*)
+			FROM		Orders
+			WHERE		EmployeeID = Employees.EmployeeID
+					AND	ShipCity = 'Paris'
+			)
+
+--2	-Pivot
+--WHERE		ShipCity IN ('Berlin', 'Paris')
+--GROUP BY	FirstName + ' ' + LastName, ShipCity
+SELECT		Name--, [Berlin], [Paris]
+FROM		(
+			SELECT		FirstName + ' ' + LastName AS Name, ShipCity, OrderID
+			FROM		Employees AS E lEFT JOIN Orders AS O
+						ON E.EmployeeID = O. EmployeeID
+			) MyOrders
+Pivot		(
+			COUNT(OrderID) FOR ShipCity IN ([Berlin], [Paris])
+			) MyReport
+WHERE		[Berlin]>[Paris]
+
+--3 -JOIN (Только Джоинами)
+SELECT		FirstName + ' ' + LastName
+FROM		Employees E LEFT JOIN Orders AS BO
+		ON	E.EmployeeID = BO.EmployeeID
+		AND	BO.ShipCity = 'Berlin'
+			LEFT JOIN Orders AS PO
+		ON	E.EmployeeID = PO.EmployeeID
+		AND	PO.ShipCity = 'Paris'
+GROUP BY	FirstName + ' ' + LastName
+HAVING		COUNT(DISTINCT BO.OrderID) > COUNT(DISTINCT PO.OrderID) 
+--Дистинкт тут потому что на 3 шаге получил таблицу заказов в квадрате
+--Потому что джоинил заказы к заказам
