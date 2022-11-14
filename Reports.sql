56fe7b516824bc3d1a4ea0a72ac4a9f31f3d71e0
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-14 04:02:14 +0300

    Update Reports.sql

--- Reports.sql
+++ Reports.sql
@@ -16,5 +16,19 @@ WHERE		[1998] > [1997]
 		AND	[1997] > [1996]
 
 
---Subtotals, Подытоги----------------------------------------------
+--WITH ROLLUP, Подытоги (Subtotals)----------------------------------------------
 
+SELECT		ShipCountry, YEAR(OrderDate), COUNT(*)
+FROM		Orders
+GROUP BY	ShipCountry, YEAR(OrderDate) WITH ROLLUP
+
+SELECT		ShipCountry, YEAR(OrderDate), COUNT(*)
+FROM		Orders
+GROUP BY	YEAR(OrderDate), ShipCountry WITH ROLLUP --Поменяли столбцы для группировки
+--Было по странам, каждый год роллапился (собирался) по стране
+--Стало что каждая страна роллапится (собирается) по году
+
+--Если добавлять в группировку столбцы, то строк будет становиться больше и будет строиться "лесенка" от последнего столбца в Group by к первому
+SELECT		ShipCountry, YEAR(OrderDate), EmployeeID,  COUNT(*)
+FROM		Orders
+GROUP BY	ShipCountry, YEAR(OrderDate), EmployeeID WITH ROLLUP --Добавили ID работника
