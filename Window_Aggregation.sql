60314dcb26e19d6fb0f9405167f0d5af6d16ce98
TwinOtak <110031123+TwinOtak@users.noreply.github.com>
2022-11-14 18:19:45 +0300

    Create Window_Aggregation.sql

---
+++ Window_Aggregation.sql
@@ -0,0 +1,10 @@
+﻿SELECT		OrderID, Freight, 
+			SUM(Freight) OVER	(
+								ORDER BY OrderID ASC
+								ROWS BETWEEN	--Задаем границы окна
+									UNBOUNDED PRECEDING	--Верхняя граница не двигается (Тут зафиксирована)
+									AND
+									CURRENT ROW			--Нижняя граница (Скользит с текущей строкой)
+								)
+FROM		Orders
+ORDER BY	OrderID
